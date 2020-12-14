c1153f8c8e09fb3f71cbead7f449cc6d7a8d88d1
Mario Manalu <70238343+mutemathy@users.noreply.github.com>
2020-12-14 07:33:23 -0600

    Implemented Newton Method Algorithm (2nd commit)

--- chapter_1/numerics1_manalu.py
+++ chapter_1/numerics1_manalu.py
@@ -1,91 +1,117 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import numerics1_manalu as num
-
-def fxn(x):
-    return np.exp(-0.5*x) + np.sin(x) - 2*x
-
-ainput = 0.0  # left endpoint
-binput = 1.0  # and right endpoint of the interval for bisection.
-xinit  = 1.0  # this is the inital estimate for fixed point iteration and newton
-
-# You can define your own functions and intervals.
-
-# change these to 1 if you want to test that section
-# set them to zero if you don't.
-run_bisection = 0
-run_plotting  = 0
-run_fixedpt   = 0
-run_newton    = 1
-
-if (run_bisection):
-  print("\n\n ****** BISECTION ***** \n")
-
-  rt, roots = num.bisection(fxn,ainput,binput)
-  print("With default tolerance, bisection gets the approximate value %0.12f." % (rt))
-  print("This took %d iterations." % (roots.size))
-
-#print(roots)
+import math
+#A function that bisects interval to find the root/roots of a function in Python
+def bisection(fxn, ainput, binput, tol = .000001, maxIter = 250):
+    #fxn is a function
+    #ainput is the lower bound
+    #binput is the upper bound
+    #tol is tolerance. That is, any midpoint in a range of size equal or smaller than the tolerance is considered a root
+    #maxIter is maximum iteration. That is, the midpoint just before the maxIter-th iteration is considered a root
 
 
-if (run_plotting):
-  t = np.arange(ainput,binput,0.001)
+    #Compute the sign of f(ainput)
+    fa = np.sign(fxn(ainput))
+    #Compute the sign of f(binput)
+    fb = np.sign(fxn(binput))
+    #Initialize iteration
+    iteration = 0
+    #Initialize roots
+    roots = np.zeros((maxIter,1))
+    #Compute initial range
+    range = binput - ainput
 
-  # plotted the function
-  plt.plot(t, fxn(t), 'r')
-  # plot the x-axis
-  plt.plot(t, 0.0*t, 'k')
-  # plot the endpoints
-  plt.plot([ainput, binput],[0.0,0.0],'go')
-  plt.ion()
-  plt.pause(0.4)
+    #If f(ainput) and f(binput) have the same signs, then there is no root in between ainput and binput
+    if fa * fb > 0:
+        raise ValueError("There is no root in this interval")
+    else:
+        #If fxn(ainput) == 0, then ainput is a root
+        if (fxn(ainput) == 0):
+            return 0, roots
+        #If fxn(binput) == 0, then binput is a root
+        elif (fxn(binput) == 0):
+            return 0, roots
 
-  inter = np.array([ainput, binput])
-  twozeros = [0.0,0.0]
-  for idx in range(roots.size):
-    # plotted the function
-    plt.plot(t, fxn(t), 'r')
-    # plot the x-axis
-    plt.plot(t, 0.0*t, 'k')
-    plt.plot(inter,twozeros,'go')
-    md = roots[idx]
-    plt.plot([md],[0.0],'bo')
-    inter = np.array([roots[idx-1], md])
-    if (idx > 5):
-      plt.xlim(0.6,0.7)
-    plt.ion()
-    plt.pause(0.4)
+    #While the range is still greater than the tolerance and iteration is less than maxIter, keep bisecting the interval
+    while (range > tol) and (iteration < maxIter):
+        #Current range is 0.5 * previous range 
+        range = 0.5 * range
+        #Compute c as the new midpoint
+        c = ainput + range
+        #Store c in the array
+        roots[iteration] = c
+        #Compute the sign function of fxn(c)
+        fc = np.sign(fxn(c))
 
-
-if (run_fixedpt):
-  print("\n\n ****** FIXED POINT ITERATION ***** \n")
-
-  # we need to convert the function above to a fixed point iteration.  Do that here:
-  def gxn(x):
-    return x + fxn(x)
-
-  tol = 1.0e-6  # default value not passed, just written for the print statment.
-  rt, roots = num.fixedpt(gxn,xinit)
-  print("With tolerance = %0.2e, FPI gets the approximate value %0.12f." % (tol,rt))
-  rt_old = rt
-  print(roots)
+        #If the signs of fxn(a) and fxn(c) are both positive or both negative, then the root is on the right half
+        if fa * fc > 0:
+            ainput = c
+        #If the signs of fxn(a) and fxn(c) are different, then the root is on the left half
+        else:
+            binput = c
+        
+        #Increment iteration
+        iteration += 1
 
 
-if (run_newton):
-  print("\n\n ****** NEWTON ***** \n")
-  # we need to define the derivative of the function for Newton's Method
-  def dfxn(x):
-    return -0.5*np.exp(-0.5*x) + np.cos(x) - 2
+    return c, roots
 
-  # now we run Newton's method, starting at the same point as FPI
-  tol = 1.0e-6  # default value not passed, just written for the print statment.
-  rt, roots = num.newton(fxn, dfxn,xinit)
-  print("With tolerance = %0.2e, FPI gets the approximate value %0.12f." % (tol,rt))
-  print("This took %d iterations." % (roots.size))
+#A function that finds a root of a function using the fixed point iteration method
+def fixedpt(fxn, xinit, tol = .000001, maxIter = 250):
+    #fxn is a function
+    #xinit is the initial approximation
+    #tol is tolerance. That is, any midpoint in a range of size equal or smaller than the tolerance is considered a root
+    #maxIter is maximum iteration. That is, the midpoint just before the maxIter-th iteration is considered a root
 
-  # we can run it again and get all 12 decimal places
-  tol = 1.0e-12  # default value not passed, just written for the print statment.
-  rt, roots = num.newton(fxn, dfxn,xinit,tol)
-  print("With tolerance = %0.2e, FPI gets the approximate value %0.12f." % (tol,rt))
-  print("This took %d iterations." % (roots.size))
-  print("\n Thought exercise: For this function with this initial estimate, what do you notice about the change in output and number of iterations and what does it mean about the result returned using the default tolerance?\n\n")
+    #Initialize iter
+    iter = 0
+    #Initialize the first root
+    r_prev = xinit
+    #Compute initial error
+    err = abs(r_prev)
+    #Initialize roots
+    roots = np.zeros((maxIter,1))
+
+    #While the range is still greater than the tolerance and iteration is less than maxIter, keep computing new roots
+    while (err > tol) and (iter < maxIter):
+        #Computes current root
+        r = fxn(r_prev)
+        #Store current root
+        roots[iter] = r
+        #Computes error
+        err = abs(r - r_prev)
+        #Set the current root to be the previous root for the next interation
+        r_prev = r
+        #Increment iter
+        iter += 1
+
+    return r, roots
+
+#A function that finds a root of a function using the Newton method
+def newton(fxn, dfxn, xinit, tol = .000001, maxIter = 250):
+    #fxn is a function
+    #dfxn is the first 
+    #tol is tolerance. That is, any r in a range of size equal or smaller than the tolerance is considered a root
+    #maxIter is maximum iteration. That is, the midpoint just before the maxIter-th iteration is considered a root   
+
+    #Initialize iter
+    iter = 0
+    #Initialize r_prev
+    r_prev = xinit
+    #Compute initial error
+    err = abs(r_prev)
+    #Initialize roots
+    roots = np.zeros((maxIter,1))
+
+    #While the error is still greater than the tolerance and iteration is less than maxIter, keep computing new roots
+    while (err > tol) and (iter < maxIter):
+        #Computes current root
+        r = r_prev - (fxn(r_prev) / dfxn(r_prev))
+        #Store the root
+        roots[iter] = r
+        #Compute new error
+        err = abs(r - r_prev)
+        #Set the current root to be the previous root for the next iteration
+        r_prev = r
+        iter += 1
+
+    return r, roots
